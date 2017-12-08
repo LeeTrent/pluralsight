7bb18aed8df74ebf9b0f6cda710c2cbc3d78e04f
LeeTrent <Lee.Trent.1@gmail.com>
2017-12-08 08:54:07 -0600

    Initial OdeToFood .NET Core project before making any changes

--- asp.net-core-fundamentals/OdeToFood/OdeToFood/Startup.cs
+++ asp.net-core-fundamentals/OdeToFood/OdeToFood/Startup.cs
@@ -27,7 +27,7 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World on Fri Dec8 8:52 AM");
             });
         }
     }

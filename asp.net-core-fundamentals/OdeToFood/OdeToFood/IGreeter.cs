7d1e32d4847301e6de2c046dc7e42963a723ee7d
LeeTrent <Lee.Trent.1@gmail.com>
2017-12-08 11:01:13 -0600

    Adds Greeter service to OdeToFood project which is used at Startup

---
+++ asp.net-core-fundamentals/OdeToFood/OdeToFood/IGreeter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string getMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        string IGreeter.getMessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}

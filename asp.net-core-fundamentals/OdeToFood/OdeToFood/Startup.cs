b0586bc7b9ed0605283b917eb7c8f81fcf084b44
LeeTrent <Lee.Trent.1@gmail.com>
2017-12-10 17:33:05 -0600

    Uses ASP.NET MVC Default Routing in OdeToFood project

--- asp.net-core-fundamentals/OdeToFood/OdeToFood/Startup.cs
+++ asp.net-core-fundamentals/OdeToFood/OdeToFood/Startup.cs
@@ -18,6 +18,8 @@ namespace OdeToFood
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,7 +60,10 @@ namespace OdeToFood
 
             //app.UseDefaultFiles();
             //app.UseStaticFiles();
-            app.UseFileServer();
+            //app.UseFileServer();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {

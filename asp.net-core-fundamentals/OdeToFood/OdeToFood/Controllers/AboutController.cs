91a3e3f3f4823e2e47a879925ce203c9b18d3463
LeeTrent <Lee.Trent.1@gmail.com>
2017-12-11 10:12:13 -0600

    Add attribute-based routing in OdeToFood project

--- asp.net-core-fundamentals/OdeToFood/OdeToFood/Controllers/AboutController.cs
+++ asp.net-core-fundamentals/OdeToFood/OdeToFood/Controllers/AboutController.cs
@@ -1,19 +1,28 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
 namespace OdeToFood.Controllers
 {
+    [Route("about")]
     public class AboutController
     {
         public AboutController()
         {
         }
+
+        [Route("")]
         public string Index()
         {
             return ( Address() + " / " + Phone() );
         }
+
+        [Route("phone")]
         public string Phone()
         {
             return "1+(777)-777-7777";
         }
+
+        [Route("address")]
         public string Address()
         {
             return "Denver, Colorado USA";

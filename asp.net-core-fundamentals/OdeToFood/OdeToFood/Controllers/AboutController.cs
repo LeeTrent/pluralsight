afc0effdfa4ba0a5cdb8af37b1529fb3189a487f
LeeTrent <Lee.Trent.1@gmail.com>
2017-12-11 09:50:16 -0600

    In OdeToFood project: 1-Uses ASP.NET MVC convention-based routing; 2-Adds AboutControler

---
+++ asp.net-core-fundamentals/OdeToFood/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace OdeToFood.Controllers
+{
+    public class AboutController
+    {
+        public AboutController()
+        {
+        }
+        public string Index()
+        {
+            return ( Address() + " / " + Phone() );
+        }
+        public string Phone()
+        {
+            return "1+(777)-777-7777";
+        }
+        public string Address()
+        {
+            return "Denver, Colorado USA";
+        }
+
+    }
+}

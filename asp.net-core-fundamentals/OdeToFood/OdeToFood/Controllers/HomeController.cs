7135a9d12a2be5878d3939be15502cb0d26b6119
LeeTrent <Lee.Trent.1@gmail.com>
2017-12-11 20:24:21 -0600

    Adds IRestaurantData and InMemoryRestaurantData implementation to render a hard-coded list of restaurants in /Views/Home/Index.cshtml

--- asp.net-core-fundamentals/OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ asp.net-core-fundamentals/OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -1,24 +1,30 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Services;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
+        private IRestaurantData _restaurantData;
+
+        public HomeController(IRestaurantData restaurantData)
         {
+            _restaurantData = restaurantData;
         }
 
         public IActionResult Index()
         {
-            var model = new Restaurant
-            { 
-                Id = 1, 
-                Name = "Linda's Vegan Kitchen"
-            };
+            //var model = new Restaurant
+            //{ 
+            //    Id = 1, 
+            //    Name = "Linda's Vegan Kitchen"
+            //};
 
+            var model = _restaurantData.GetAll();
             return View(model);
+
             //return new ObjectResult(model);
             //return Content("Hello from HomeController.Index(), returning IActionResult and extending base Controller");
         }
---
+++ asp.net-core-fundamentals/OdeToFood/OdeToFood/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        // CAUTION - NOT THREAD SAFE!!
+        private List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "Restuarant A" },
+                new Restaurant { Id = 2, Name = "Restaurant B" },
+                new Restaurant { Id = 3, Name = "Restaurant C" },
+                new Restaurant { Id = 4, Name = "Restaurant D" },
+                new Restaurant { Id = 5, Name = "Restaurant E" }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            // CAUTION - NOT THREAD SAFE!!
+            return _restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
